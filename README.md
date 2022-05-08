2114635b45a18ff42ee61b1166c924dd0c94224c
XuanJiAndroid <gxj1236987450@gmail.com>
2022-05-09 00:45:57 +0800

    feat: 初始化项目

---
+++ README.md
@@ -0,0 +1,8 @@
+# MotionLayout用例展示
+仿PP虾的视频播放布局，整体联动产生变化：
+
+用例的特点：
+
+1. 基础的MotionLayout使用API展示
+2. 多级联动的MotionLayout的用法展示
+
---
+++ app/src/main/java/com/gxj/motionlayoutdemo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gxj.motionlayoutdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
